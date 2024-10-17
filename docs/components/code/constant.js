c3056c221ea72475287a3ffd731f2432e8ab109c
AlanQtten <735814735@qq.com>
2024-10-17 17:13:16 +0800

    fix: remove useless js output file

--- docs/components/code/constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.propsMergeSymbol = exports.lineIndexSymbol = void 0;
-exports.lineIndexSymbol = Symbol('lineIndex');
-exports.propsMergeSymbol = Symbol('mergeProps');
--- docs/components/quiz/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getQuizName = getQuizName;
-var quizNamePre = 'quiz-pre-';
-var quizNameId = 0;
-function getQuizName() {
-    return "".concat(quizNamePre).concat(quizNameId++);
-}
